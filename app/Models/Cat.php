56c660544d1d14ee9a083027aaa616cac79e224d
AdninAli01 <adnin.alihud@gmail.com>
2024-05-09 11:35:51 +0800

    model and seend

--- app/Models/Cat.php
+++ app/Models/Cat.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class cat extends Model
+class Cat extends Model
 {
     use HasFactory;
 
